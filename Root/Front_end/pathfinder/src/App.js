3da909fc568cb595806e7307bcc3b4087d9800eb
JungWooHwang1 <xhdpc123@naver.com>
2024-09-19 15:56:40 +0900

    ICON 라이브러리 추가

--- Root/Front_end/pathfinder/src/App.js
+++ Root/Front_end/pathfinder/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import PF_Main from './HTML/PF_Main';
+import logo from "./logo.svg";
+import "./App.css";
+import PF_Main from "./HTML/PF_Main";
+import LoginForm from "./HTML/LoginForm";
 
 function App() {
   return (
     <div className="App">
-        <PF_Main/>
+      <LoginForm />
     </div>
   );
 }
