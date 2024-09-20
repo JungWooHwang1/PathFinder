45e884780149b12ab903a23364b18e368be38455
JungWooHwang1 <xhdpc123@naver.com>
2024-09-20 15:55:18 +0900

    메인 업뎃

--- Root/Front_end/pathfinder/src/App.js
+++ Root/Front_end/pathfinder/src/App.js
@@ -1,22 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import PF_Main from "./HTML/PF_Main";
-<<<<<<< HEAD
-import Write from "./HTML/PF_Write";
-import PF_Lost from "./HTML/PF_Lost";
-import PF_Find from "./HTML/PF_Find";
-function App() {
-  return (
-    <div className="App">
-      <PF_Lost />
-=======
-import LoginForm from "./HTML/PF_LoginForm";
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
->>>>>>> 77beef981c3e4a3e98edb1fc49797d557d3db424
+      <PF_Main />
     </div>
   );
 }
